f2a3cb0636466ff6b5ca70b0d865cccf4f9806ae
Matt Pilott <matt@pilott.me>
2026-06-01 11:07:03 +0100

    fix url

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch }) {
-   let url = 'https://cdn.statically.io/gh/mdn/data/38bc01a942de230a513963f643c51eb22827e738/css/syntaxes.json'
+   let url = 'https://cdn.jsdelivr.net/npm/mdn-data/css/syntaxes.json'
 
    // prettier-ignore
    const { 'named-color': { syntax: colours } } = await fetch(url).then(r => r.json())
